4e88a01d94d10dc3cded477946cddaed1b2df6dc
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-04-03 17:57:21 -0300

    Include nodemon and Swagger for API interactions

---
+++ src/app/swagger.js
@@ -0,0 +1,20 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Sistema de Gestión de Alquiler de Productos de Playa',
+            description: 'Para un parador en el caribe',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./src/**/*.js'], // Path to the API docs
+};
+
+const specs = swaggerJsdoc(options);
+
+export default (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
