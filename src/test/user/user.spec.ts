b68dabb3f4dfff9e1ce5ac23f87401c1f5efc88e
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-04-03 20:41:46 -0300

    Implement UnitTesting with Vitest

---
+++ src/test/user/user.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest'
+import User from "../../app/model/User.js";
+
+test('Verificar user email', () => {
+    const user:User = new User();
+    expect(user.getEmail()).equal("", "El email no es correcto");
+})
