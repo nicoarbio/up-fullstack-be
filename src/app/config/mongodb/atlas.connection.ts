65bec3e10754e11af892cb95bd9d45b6d28dbf9c
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-04-11 03:14:36 +0000

    Seed atlas DB

--- src/app/config/mongodb/atlas.connection.ts
+++ src/app/config/mongodb/atlas.connection.ts
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 import { DB_CONFIG } from "@config/config.properties";
 import { mongoClientOptions } from "@config/db.config";
+import { seedDatabase } from "@config/mongodb/inmemory.connection";
 
 export default async function connectToAtlasMongoDB()  {
     const uri = DB_CONFIG.getConnectionString();
 
     return mongoose.connect(uri, mongoClientOptions).then(
-        () => {
+        async () => {
             console.log('MongoDB Atlas successfully connected');
+            await seedDatabase();
         },
         (err) => {
             console.error('Error connecting to MongoDB Atlas', err);
