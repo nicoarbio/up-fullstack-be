45f42d2a893845a7145a6f50a661f7d5d040e581
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-04-12 06:56:17 +0000

    Add auth libs

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,9 @@ export default {
         globals: true,
         environment: 'node',
         env: {
-            DB_SEED_PATH: "src/test/config/jsonseed"
+            DB_SEED_PATH: "src/test/config/jsonseed",
+            JWT_SECRET_ACCESS: "e76352c2968412a03102dfc1bd067261",
+            JWT_SECRET_REFRESH: "fae59e528d30fd9adb150e1eb7a79a35"
         }
     },
 };
