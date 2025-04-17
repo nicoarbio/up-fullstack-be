7b35a446b5393b448f90dbe0e97ab363f578ba8a
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-04-17 02:28:49 +0000

    Update user profile

--- src/app/controller/profile.controller.ts
+++ src/app/controller/profile.controller.ts
@@ -1,13 +1,33 @@
 import { Request, Response } from "express";
 import { User } from "@model/user.model";
 
+const userProfileSelect = "-_id -passwordHash -__v";
+
 export async function getUserProfile(req: Request, res: Response) {
-    await User.findById(req.user?.id).select("-_id -passwordHash -__v")
+    await User.findById(req.user?.id).select(userProfileSelect)
         .then(user => {
             res.status(200).json(user);
             console.log(`User profile retrieved successfully. [${JSON.stringify(user)}]`);
         }).catch(error => {
             res.status(500).json({ error: error.message, detail: error.cause });
             console.error("Error retrieving user profile:", error.message || error);
+        })
+}
+
+export async function updateUserProfile(req: Request, res: Response) {
+    const fieldsToUpdate = {
+        name: req.body.name,
+        lastname: req.body.lastname,
+        phoneNumber: req.body.phoneNumber,
+    };
+    const userId = req.user!.id;
+
+    await User.findByIdAndUpdate(userId, fieldsToUpdate, { new: true }).select(userProfileSelect)
+        .then(user => {
+            res.status(200).json(user);
+            console.log(`User profile updated successfully. [${JSON.stringify(user)}]`);
+        }).catch(error => {
+            res.status(500).json({ error: error.message, detail: error.cause });
+            console.error("Error updating user profile:", error.message || error);
         })
 }
--- src/app/route/profile.routes.ts
+++ src/app/route/profile.routes.ts
@@ -1,6 +1,25 @@
 import { Router } from "express";
-import { getUserProfile } from "@controller/profile.controller";
+import { getUserProfile, updateUserProfile } from "@controller/profile.controller";
 import { authenticate } from "@middleware/authentication.middleware";
+import { withValidation } from "@middleware/validateRequest.middleware";
+import { body } from 'express-validator';
+
+export const profileUpdateValidation = [
+    body('name')
+        .optional()
+        .isString().withMessage('El nombre debe ser una cadena')
+        .isLength({ min: 1 }).withMessage('El nombre no puede estar vacío'),
+
+    body('lastname')
+        .optional()
+        .isString().withMessage('El apellido debe ser una cadena')
+        .isLength({ min: 1 }).withMessage('El apellido no puede estar vacío'),
+
+    body('phoneNumber')
+        .optional()
+        .isString().withMessage('El teléfono debe ser una cadena'),
+];
 
 export default Router()
-    .get("/profile", authenticate, getUserProfile);
+    .get("/profile", authenticate, getUserProfile)
+    .put("/profile", authenticate, withValidation(profileUpdateValidation), updateUserProfile);
